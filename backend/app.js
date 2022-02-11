e31c390f38fa550a8d771bb16f39dc46c51c77f5
HemantRamawat <hramawat4@gmail.com>
2022-02-11 11:45:52 +0530

    changes

--- backend/app.js
+++ backend/app.js
@@ -11,9 +11,9 @@ const order = require('./routes/orderRoute');
 
  const path = require('path');
 
- if(process.env.NODE_ENV!=="PRODUCTION"){
+//  if(process.env.NODE_ENV!=="PRODUCTION"){
     require('dotenv').config({path:"backend/config/config.env"})
-  }
+  // }
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -26,8 +26,8 @@ app.use(errorMiddleware);
 app.use("/api/v1",user);
 app.use("/api/v1",order)
 app.use("/api/v1",payment)
-app.use(express.static(path.join(__dirname,"../frontend/build")))
-app.get("*",(req,res) => {
-    res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
-})
+// app.use(express.static(path.join(__dirname,"../frontend/build")))
+// app.get("*",(req,res) => {
+//     res.sendFile(path.resolve(__dirname,"../frontend/build/index.html"));
+// })
 module.exports =app;
